e351e13853bbcc38a2d7d5d901d315a8fc45baf5
Nebarg <subs@woode.co.uk>
2022-07-17 23:46:29 +0100

    Update SSH key gen file to ask for email address if not present

--- gens.sh
+++ gens.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 echo "Generating a new SSH key for GitHub..."
+EMAIL=$1
+if [[ ! $EMAIL ]]
+then
+    read -p "Enter email address: " EMAIL
+fi
 
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
-ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
+ssh-keygen -t ed25519 -C $EMAIL -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
